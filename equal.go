50ec2081a34c12bead889c3afe30b70b2faf56c8
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-04-30 14:01:11 -0300

    feat: Adding StrictEqual function to assert strict equality
    This commit introduces a new `StrictEqual` function to the assert package. This function performs a strict equality check using `reflect.DeepEqual` to ensure that the result and expected values are deeply equal, including their types.
    
    The existing `Equal` function now uses `fmt.Sprint` to compare values, allowing for equality checks between different types that have the same string representation.
    
    This change provides more flexibility in assertion testing, allowing developers to choose between strict and non-strict equality checks based on their specific needs.

--- equal.go
+++ equal.go
@@ -1,6 +1,7 @@
 package assert
 
 import (
+	"fmt"
 	"reflect"
 )
 
@@ -8,7 +9,7 @@ import (
 // If the result is not equal to the expected value, it reports a fatal error
 // using the provided fatalMessage.
 //
-//	result := someFunction()
+//	var result int = someFunction()
 //	expected := 42
 //	assert.Equal(t, result, expected, "Expected result to be 42")
 func Equal(t T, result, expected any, args ...any) {
@@ -19,7 +20,26 @@ func Equal(t T, result, expected any, args ...any) {
 	}
 }
 
+// StrictEqual checks if the provided result is equal to the expected value.
+// If the result is not equal to the expected value, it reports a fatal error
+// using the provided fatalMessage.
+//
+//	type value int
+//	assert.StrictEqual(t, value(10), 10, "not equal")// not equal
+//	assert.StrictEqual(t, value(20), value(10), "not equal")// not equal
+//	assert.StrictEqual(t, value(20), value(20), "not equal")// pass
+func StrictEqual(t T, result, expected any, args ...any) {
+	tester := initTest(t)
+	configureTest(tester, result, expected)
+	if !strictEqual(result, expected) {
+		tester.Fatal(args...)
+	}
+}
+
+func strictEqual(result, expected any) bool {
+	return reflect.DeepEqual(result, expected)
+}
 
 func equal(result, expected any) bool {
-	return reflect.DeepEqual(result, expected)
+	return fmt.Sprint(result) == fmt.Sprint(expected)
 }
